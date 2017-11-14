397e7dda2b8c4aad7ecc7d87532efd8c70b2ad84
PJ <jardiolin.paolo@gmail.com>
2017-11-13 21:57:14 -0800

    bound interface to array

--- js/main.js
+++ js/main.js
@@ -17,23 +17,34 @@ function Board(size){
     // Build it onto page
     let render = function render(){
         for(let i = 0; i < arr.length; i++){
+            let row = document.createElement('DIV');
+            row.className = 'row';
             for(let j = 0; j < arr[i].length; j++){
                 let tile = document.createElement('DIV');
-                tile.className = 'tile';
-                tile.textContent = arr[i][j];
-                container.appendChild(tile);
+                tile.className = 'tile ' + i.toString() + 'x' + j.toString() + 'y';
+                tile.textContent = /*arr[i][j]*/i.toString() +','+ j.toString();
+                tile.addEventListener('click', update, false);
+                //tile.onclick = console.log('clicked coordinate: ');
+                row.appendChild(tile);
             }
+            container.appendChild(row);
         }
         document.body.appendChild(container);
     }
 
     // update after player makes a move
     let update = function update(){
-        render();        
+        this.textContent = 'X'; // map to player instead
+        let x = this.className.toString().match(/\d(?=x)/)[0];
+        let y = this.className.toString().match(/\d(?=y)/)[0];
+        arr[x][y] = this.textContent;
+        console.log(x + ',' + y);
+        printBoard();
     }
 
-    this.print = function(){
-        return arr;
+    printBoard = function(){
+        console.log(arr);
+        //return arr;
     }
 
     this.getSize = function(){
@@ -45,6 +56,6 @@ function Board(size){
 
 let ticTacToe = new Board(3);
 console.log(ticTacToe.getSize());
-console.log(ticTacToe.print());
+//console.log(ticTacToe.print());
 //console.log(ticTacToe.render());
 //console.log(ticTacToe);
